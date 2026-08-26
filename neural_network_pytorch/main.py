a2491e5831dd4ef2c9e7edf8eddc649cafcd7c7f
Kenneth Packard <kenpack6@byu.edu>
2026-08-26 14:53:29 -0600

    Moved data_load() into the model class and adjusted training_loop()

--- neural_network_pytorch/main.py
+++ neural_network_pytorch/main.py
@@ -1,7 +1,6 @@
 #This is to do some more practice with PyTorch as well.
 import torch
 from neural_network_class import Neural_Network
-from torch.utils.data import TensorDataset, DataLoader
 import pickle
 import gzip
 from pathlib import Path
@@ -9,43 +8,10 @@ from pathlib import Path
 CURRENT_DIR = Path(__name__).resolve().parent
 DATA_PATH = CURRENT_DIR.parent / "dataset" / "mnist.pkl.gz"
 
-def load_data(data_path):
-    """Loads data and return two dictionaries containing datasets and loaders"""
-    with gzip.open(data_path, "r") as file:
-        train_data, validation_data, test_data = pickle.load(file, encoding="latin1")
-        print(type(train_data))
-    
-    #Convert to Tensor DataSet
-    
-    raw_dict = {"train": train_data,
-                "validation": validation_data,
-                "test": test_data}
-    
-    tensor_data_dict = {}
-    for name, data in raw_dict.items():
-        #Split labels and images
-        images, labels = data
-        tensor_data_dict[name] = TensorDataset(torch.tensor(images, dtype=torch.float32),
-                                               torch.tensor(labels,dtype=torch.long))
-        
-    # Define DataLoaders
-    data_loader_dict = {}
-    for name, item in tensor_data_dict.items():
-        loader = DataLoader(item,batch_size=32,shuffle=True)
-        data_loader_dict[name] = loader
-        
-    return tensor_data_dict, data_loader_dict
-    
-
-
-def main():
-    datasets, loaders = load_data(DATA_PATH)
-    
-    training_data = loaders["train"]
-    
+def main():    
     nn_model = Neural_Network()
     
-    nn_model.training_loop(training_data)
+    nn_model.training_loop(DATA_PATH, 1)
     
 
 if __name__ == "__main__":
--- neural_network_pytorch/neural_network_class.py
+++ neural_network_pytorch/neural_network_class.py
@@ -3,6 +3,10 @@ import torch.nn as nn
 import torch.autograd 
 import torch.optim 
 import torch.nn.functional as F
+from torch.utils.data import TensorDataset, DataLoader
+import pickle
+import gzip
+from pathlib import Path
 
 #I want to copy the implementation from 3Blue1Brown, so we have 784 inputs and two hidden layers with 19 neurons and 10 outputs
 
@@ -37,7 +41,7 @@ class Neural_Network(nn.Module):
         #Hidden Layers
         hidden_layers = [self.layer1, self.layer2]
         for layer in hidden_layers:
-            layer(x) = x
+            x = layer(x)
             
         #Output Layer
         x = self.layer4(x)
@@ -51,21 +55,51 @@ class Neural_Network(nn.Module):
         output = loss_x
         
         return output
-    
-    def training_loop(self, data,y_labels,epochs):
+
+    def load_data(self, data_path):
+        """Loads data and return two dictionaries containing datasets and loaders"""
+        with gzip.open(data_path, "r") as file:
+            train_data, validation_data, test_data = pickle.load(file, encoding="latin1")
+            print(type(train_data))
+        
+        #Convert to Tensor DataSet
+        
+        raw_dict = {"train": train_data,
+                    "validation": validation_data,
+                    "test": test_data}
+        
+        tensor_data_dict = {}
+        for name, data in raw_dict.items():
+            #Split labels and images
+            images, labels = data
+            tensor_data_dict[name] = TensorDataset(torch.tensor(images, dtype=torch.float32),
+                                                torch.tensor(labels,dtype=torch.long))
+            
+        # Define DataLoaders
+        data_loader_dict = {}
+        for name, item in tensor_data_dict.items():
+            loader = DataLoader(item,batch_size=32,shuffle=True)
+            data_loader_dict[name] = loader
+            
+        return tensor_data_dict, data_loader_dict
+
+    def training_loop(self,data_path,epochs):
+        
+        __, loaders = self.load_data(self, data_path)
         
         for i in range(0,epochs):
-            
-            print(f"Running epoch: {i}")
-            loss = self.forward(self, data, y_labels)
-            print(f"Loss({i}): {loss}")
-            
-            #Backward Pass
-            loss.backward()
-            
-            #Update Gradients
-            self.optimizer.step()
-            self.optimizer.zero_grad()
+            for batch in loaders:
+                data, y_labels = batch
+                print(f"Running epoch: {i}")
+                loss = self.forward(self, data, y_labels)
+                print(f"Loss({i}): {loss}")
+                
+                #Backward Pass
+                loss.backward()
+                
+                #Update Gradients
+                self.optimizer.step()
+                self.optimizer.zero_grad()
         
         torch.save(self.state_dict(), "mnist_model.pth")
             
