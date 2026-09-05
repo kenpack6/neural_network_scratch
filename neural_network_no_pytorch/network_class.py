12d21059c3831bcbcc5316b6732192811a2b787f
Kenneth Packard <kenpack6@byu.edu>
2026-09-05 15:31:34 -0600

    Rewriting backprop algorithm

--- neural_network_no_pytorch/network_class.py
+++ neural_network_no_pytorch/network_class.py
@@ -24,26 +24,6 @@ class Network:
                 layer.update(learning_rate)
     
     
-    def back_prop(self, predicted, true_labels):
-        
-        current_dout = (predicted - true_labels) / predicted.shape[0]
-        
-        for layer in reversed(self.layers):
-            current_dout =  layer.backward(current_dout)
-            
-            
-            
-        
-        
-        
-        
-        
-        
-        
-    
-    
-    #1 Store the layers
-    #2 Forward pass method
-    #3 Loss function
-    #4 Backprop
-    #5 Adjust weights based on gradients
+    def back_prop():
+        pass
+   
